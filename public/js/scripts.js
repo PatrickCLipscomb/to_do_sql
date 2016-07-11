9c7c60715f089db9ae32c4c41cb883c285ae3219
matthew giem and patrick lipscomb <patrick@>
2016-07-11 16:50:22 -0700

    will add jquery functionality for a cleaner experience.

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(event) {
+  $('#new_list_toggle').on('click', function() {
+    $('#new_list_name').show();
+  });
+  $('#new_list_hide').on('click', function() {
+    $('#new_list_name').hide();
+  });
+})
